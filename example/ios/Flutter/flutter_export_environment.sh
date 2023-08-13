6de5f64b19d88c9fa250d32283166ca5f88f3f21
smocken <stefan@mocken.org>
2023-08-13 17:50:08 +0200

    added month and year picker

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/opt/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/smocken/flutter-git/date_picker_timetable/example"
+export "FLUTTER_APPLICATION_PATH=/Users/smocken/development/flutter-git/DatePickerTimetable/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
--- lib/extra/style.dart
+++ lib/extra/style.dart
@@ -2,6 +2,12 @@ import 'package:date_picker_timetable/extra/color.dart';
 import 'package:flutter/material.dart';
 import 'package:date_picker_timetable/extra/dimen.dart';
 
+const TextStyle defaultYearTextStyle = TextStyle(
+  color: AppColors.defaultYearColor,
+  fontSize: Dimen.yearTextSize,
+  fontWeight: FontWeight.w500,
+);
+
 const TextStyle defaultMonthTextStyle = TextStyle(
   color: AppColors.defaultMonthColor,
   fontSize: Dimen.monthTextSize,
@@ -18,4 +24,10 @@ const TextStyle defaultDayTextStyle = TextStyle(
   color: AppColors.defaultDayColor,
   fontSize: Dimen.dayTextSize,
   fontWeight: FontWeight.w500,
+);
+
+const TextStyle defaultSelectedMonthTextStyle = TextStyle(
+  color: AppColors.defaultSelectedMonthColor,
+  fontSize: Dimen.selectedMonthTextSize,
+  fontWeight: FontWeight.bold,
 );
